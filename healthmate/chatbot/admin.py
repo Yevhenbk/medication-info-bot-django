049b82fc9e666238d1ce705f011fa3ea5f230941
Yevhenbk <balagutrak.jenia@gmail.com>
2024-02-13 15:55:01 +0100

    clean code

--- healthmate/chatbot/admin.py
+++ healthmate/chatbot/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import TodoItem, Chat, Message
+from .models import Chat, Message
 
-admin.site.register(TodoItem)
+
 admin.site.register(Chat)
 admin.site.register(Message)
